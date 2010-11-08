46f34e69c0044eb9e4fad9cbda0707e0816e1dfd
hoodie <hoodie@9a665d2c-7092-0410-b790-bb0f4d5e5a57>
2010-11-08 13:35:21 +0000

    more dummy
    git-svn-id: https://gforge.zih.tu-dresden.de/svn/c0h0@108 9a665d2c-7092-0410-b790-bb0f4d5e5a57

--- module/c0h0/gui/Clickdummy.java
+++ module/c0h0/gui/Clickdummy.java
@@ -1,11 +1,17 @@
 package org.jalgo.module.c0h0.gui;
 
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Clickdummy extends JPanel{
+	public JPanel left;
+	public BoxLayout leftright;
+	
 	public Clickdummy() {
+		leftright = new BoxLayout(this, BoxLayout.Y_AXIS);
 		JLabel test = new JLabel("Foo");
+		setLayout(leftright);
 		add(test);
 	}
 }
