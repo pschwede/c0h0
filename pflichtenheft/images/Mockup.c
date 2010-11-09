33e3ae30c054910033d2e825496c602fde02e66e
hoodie <hoodie@9a665d2c-7092-0410-b790-bb0f4d5e5a57>
2010-11-09 10:15:59 +0000

    added pflichtenheft/images - again
    git-svn-id: https://gforge.zih.tu-dresden.de/svn/c0h0@126 9a665d2c-7092-0410-b790-bb0f4d5e5a57

---
+++ pflichtenheft/images/Mockup.c
@@ -0,0 +1,16 @@
+int main()
+{
+	int x1, x2, ... , xm;
+	scanf("%i", &x1);
+	scanf("%i", &x2);
+	...
+	scanf("%i", &xm);
+ 
+	if(x1 > 4)
+	{
+		x1 = x1 -1
+	}
+ .....
+	printf("%d", xi);
+	Areturn 0;
+}
