1a172d5aa3bfc9125bdd66f2adff1a69ec9e8b43
hoodie <hoodie@9a665d2c-7092-0410-b790-bb0f4d5e5a57>
2010-11-09 12:51:49 +0000

    very important changes
    git-svn-id: https://gforge.zih.tu-dresden.de/svn/c0h0@132 9a665d2c-7092-0410-b790-bb0f4d5e5a57

--- module/c0h0/gui/Clickdummy.java
+++ module/c0h0/gui/Clickdummy.java
@@ -22,6 +22,8 @@ import javax.swing.text.Document;
 import javax.swing.text.EditorKit;
 import javax.swing.text.ViewFactory;
 
+import org.jalgo.module.c0h0.views.C0View;
+
 
 public class Clickdummy extends JPanel {
 	JPanel right;
@@ -34,12 +36,13 @@ public class Clickdummy extends JPanel {
 
 	public Clickdummy() {
 
+		C0View c0view = new C0View(c0edit);
+		
 		// panels
 		left = new JPanel();
 		right = new JPanel();
 		bottom = new JPanel();
-		c0edit = new JEditorPane("text/html",
-	            "<html>\n<body  style=\"margin:0;padding:0;font-family:'sans-serif,courier-new,monospace';\">\n<div style=\"color:#181615;padding:0px;margin:0px;\"><div style='background-color:#cccccc;padding:0px;margin:0px;'><span style='background-color:#cccccc;'>01</span> <span style='color:#0057ae;'><b>int</b></span> main()<br />\n<span style='background-color:#cccccc;'>02</span> {<br />\n<span style='background-color:#cccccc;'>03</span> &nbsp;&nbsp;<span style='color:#0057ae;'><b>int</b></span> x1, x2, ... , xm;<br />\n<span style='background-color:#cccccc;'>04</span> &nbsp;&nbsp;<b>scanf</b>(<span style='color:#bf0303;'>&quot;%i&quot;</span>, &amp;x1);<br />\n<span style='background-color:#cccccc;'>05</span> &nbsp;&nbsp;<b>scanf</b>(<span style='color:#bf0303;'>&quot;%i&quot;</span>, &amp;x2);<br />\n<span style='background-color:#cccccc;'>06</span> &nbsp;&nbsp;...<br />\n<span style='background-color:#cccccc;'>07</span> &nbsp;&nbsp;<b>scanf</b>(<span style='color:#bf0303;'>&quot;%i&quot;</span>, &amp;xm);</div><span style='background-color:#cccccc;'>08</span><div style='color:#181615;background-color:#ffdddd;padding:0px;margin:0px;'><span style='background-color:#cccccc;'>09</span> &nbsp;&nbsp;<b>if</b>(x1 &gt; <span style='color:#b08000;'>4</span>)<br />\n<span style='background-color:#cccccc;'>10</span> &nbsp;&nbsp;{<br />\n<span style='background-color:#cccccc;'>11</span> &nbsp;&nbsp;&nbsp;&nbsp;x1 = x1 -<span style='color:#b08000;'>1</span><br />\n<span style='background-color:#cccccc;'>12</span> &nbsp;&nbsp;}</div>\n<span style='background-color:#cccccc;'>13</span> &nbsp;&nbsp;<b>printf</b>(<span style='color:#bf0303;'>&quot;%d&quot;</span>, xi);<br />\n<span style='background-color:#cccccc;'>14</span> &nbsp;&nbsp;<b>return</b> <span style='color:#b08000;'>0</span>;<br />\n<span style='background-color:#cccccc;'>15</span> }<br />\n</div>\n</body>\n</html>");
+		c0edit = new JEditorPane("text/html",c0view.plainText());
 		c0edit.setSize(500, 500);
 		leftscroll = new JScrollPane(c0edit);
 		//c0edit.setEditorKit(new Numbered)
