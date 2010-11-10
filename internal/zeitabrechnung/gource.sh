e4bd4ab634348f97b3f11f8dd760a262fb32a1f6
spazzpp2 <spazzpp2@9a665d2c-7092-0410-b790-bb0f4d5e5a57>
2010-11-10 15:24:41 +0000

    packages im ekd
    git-svn-id: https://gforge.zih.tu-dresden.de/svn/c0h0@151 9a665d2c-7092-0410-b790-bb0f4d5e5a57

--- internal/zeitabrechnung/gource.sh
+++ internal/zeitabrechnung/gource.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gource --disable-progress --disable-bloom --stop-at-end -640x480 --output-ppm-stream - | ffmpeg -y -b 3000K -r 60 -f image2pipe -vcodec ppm -i - gource.mp4
+gource --camera-mode track --disable-progress --disable-bloom --stop-at-end -640x480 --output-ppm-stream - | ffmpeg -y -b 3000K -r 60 -f image2pipe -vcodec ppm -i - gource.mp4
